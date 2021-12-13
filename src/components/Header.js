34f3b5462debf9d37a914385cb49f142a2912d2a
Hakan <hakan9719@gmail.com>
2021-12-13 01:03:42 +0100

    init amazon project

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+export default function Header() {
+  return (
+    <header>
+      {/* Top Nav  */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 ">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="fill"
+            className="cursor-pointer"
+          />
+        </div>
+
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* TODO Right side time line 50:35 yt */}
+      </div>
+
+      {/* Bottom Nav  */}
+      <div></div>
+    </header>
+  );
+}
